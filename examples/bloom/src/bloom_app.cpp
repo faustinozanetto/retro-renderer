5b29ff65e654d03e6e495f74903efc0a86bebf85
Faustino Zanetto <faustizanetto@gmail.com>
2023-06-07 13:08:27 -0300

    feat(core): added new base assets for the example projects and got basic bloom app working

--- examples/bloom/src/bloom_app.cpp
+++ examples/bloom/src/bloom_app.cpp
@@ -6,9 +6,10 @@
 bloom_app::bloom_app() : application("./")
 {
     load_shaders();
-    load_texture();
     setup_model();
     setup_camera();
+    setup_frame_buffers();
+    setup_screen_quad();
 }
 
 bloom_app::~bloom_app()
@@ -17,31 +18,56 @@ bloom_app::~bloom_app()
 
 void bloom_app::on_update()
 {
-    m_shader->bind();
+    glDisable(GL_BLEND);
+    // 1. Render to geometry fbo
+    m_geometry_fbo->bind();
+    retro::renderer::renderer::clear_screen();
+    m_geometry_shader->bind();
     glm::mat4 model = glm::mat4(1.0f);
+    model = glm::scale(model, glm::vec3(2.0f));
     model = glm::rotate(model, static_cast<float>(glfwGetTime()), {0, 1, 0});
-    m_shader->set_mat4("u_transform", model);
-    m_shader->set_mat4("u_view", m_camera->get_view_matrix());
-    m_shader->set_mat4("u_projection", m_camera->get_projection_matrix());
-    retro::renderer::renderer::bind_texture(0, m_texture->get_handle_id());
+    m_geometry_shader->set_mat4("u_transform", model);
+    m_geometry_shader->set_mat4("u_view", m_camera->get_view_matrix());
+    m_geometry_shader->set_mat4("u_projection", m_camera->get_projection_matrix());
+    m_material->bind(m_geometry_shader);
     retro::renderer::renderer::submit_model(m_model);
-    m_shader->un_bind();
+    m_geometry_shader->un_bind();
+    m_geometry_fbo->un_bind();
+
+    // 2. Render geometry result using pbr to screen
+    // m_lighting_fbo->bind();
+    retro::renderer::renderer::clear_screen();
+    m_pbr_shader->bind();
+    m_pbr_shader->set_vec_float3("u_cam_pos", m_camera->get_position());
+    m_pbr_shader->set_vec_float3("u_directional_light.position", {0.0f, 0.0f, 0.0f});
+    m_pbr_shader->set_vec_float3("u_directional_light.direction", {1.8f, -5.5f, -3.6f});
+    m_pbr_shader->set_vec_float3("u_directional_light.color", {10.0f, 10.0f, 10.0f});
+    retro::renderer::renderer::bind_texture(0, m_geometry_fbo->get_attachment_id(0)); // Position
+    retro::renderer::renderer::bind_texture(1, m_geometry_fbo->get_attachment_id(1)); // Albedo
+    retro::renderer::renderer::bind_texture(2, m_geometry_fbo->get_attachment_id(2)); // Normal
+    retro::renderer::renderer::bind_texture(3, m_geometry_fbo->get_attachment_id(3)); // Roughness - Metallic - AO
+    retro::renderer::renderer::bind_texture(4, m_geometry_fbo->get_attachment_id(4)); // Emissive
+    m_screen_vao->bind();
+    retro::renderer::renderer::submit_elements(GL_TRIANGLES, 6);
+    m_screen_vao->un_bind();
+    m_pbr_shader->un_bind();
+    // m_lighting_fbo->un_bind();
 }
 
 void bloom_app::load_shaders()
 {
-    m_shader = retro::renderer::shader_loader::load_shader_from_file(
-        "../resources/shaders/model-textured.rrs");
-}
-
-void bloom_app::load_texture()
-{
-    m_texture = retro::renderer::texture_loader::load_texture_from_file("../resources/models/tv/tv-albedo.png");
+    m_geometry_shader = retro::renderer::shader_loader::load_shader_from_file(
+        "../resources/shaders/geometry.rrs");
+    m_pbr_shader = retro::renderer::shader_loader::load_shader_from_file(
+        "../resources/shaders/pbr.rrs");
+    m_screen_shader = retro::renderer::shader_loader::load_shader_from_file(
+        "../resources/shaders/screen.rrs");
 }
 
 void bloom_app::setup_model()
 {
-    m_model = retro::renderer::model_loader::load_model_from_file("../resources/models/tv/tv.obj");
+    m_model = retro::renderer::model_loader::load_model_from_file("../resources/models/radio/radio.obj");
+    m_material = retro::renderer::material_loader::load_material_from_file("../resources/materials/radio.rrm");
 }
 
 void bloom_app::setup_camera()
@@ -50,12 +76,128 @@ void bloom_app::setup_camera()
     m_camera->set_position({0.0f, 0.5f, 12.0f});
 }
 
+void bloom_app::setup_screen_quad()
+{
+    const std::vector<float> quad_vertices = {
+        1.0f, 1.0f, 0.0f, 1.0f, 1.0f,   // top right
+        1.0f, -1.0f, 0.0f, 1.0f, 0.0f,  // bottom right
+        -1.0f, -1.0f, 0.0f, 0.0f, 0.0f, // bottom left
+        -1.0f, 1.0f, 0.0f, 0.0f, 1.0f   // top left
+    };
+
+    const std::vector<uint32_t> indices = {
+        0, 3, 1, // first triangle
+        1, 3, 2, // second triangle
+    };
+
+    size_t vertex_buffer_size = quad_vertices.size() * sizeof(&quad_vertices[0]);
+    size_t index_buffer_size = indices.size() * sizeof(&indices[0]);
+
+    m_screen_vao = std::make_shared<retro::renderer::vertex_array_object>();
+    std::shared_ptr<retro::renderer::vertex_buffer_object> vertices_vbo = std::make_shared<
+        retro::renderer::vertex_buffer_object>(retro::renderer::vertex_buffer_object_target::arrays);
+
+    std::shared_ptr<retro::renderer::vertex_buffer_object> index_buffer = std::make_shared<
+        retro::renderer::vertex_buffer_object>(retro::renderer::vertex_buffer_object_target::elements, indices.size());
+
+    m_screen_vao->bind();
+    vertices_vbo->bind();
+    vertices_vbo->set_data(retro::renderer::vertex_buffer_object_usage::static_draw, vertex_buffer_size,
+                           quad_vertices.data());
+
+    index_buffer->bind();
+    index_buffer->set_data(retro::renderer::vertex_buffer_object_usage::static_draw, index_buffer_size, indices.data());
+
+    std::initializer_list<retro::renderer::vertex_buffer_layout_entry>
+        layout_elements = {
+            {"a_pos", retro::renderer::vertex_buffer_entry_type::vec_float3, false},
+            {"a_tex_coord", retro::renderer::vertex_buffer_entry_type::vec_float2, false},
+        };
+
+    std::shared_ptr<retro::renderer::vertex_buffer_layout_descriptor>
+        vertices_vbo_layout_descriptor = std::make_shared<retro::renderer::vertex_buffer_layout_descriptor>(
+            layout_elements);
+    vertices_vbo->set_layout_descriptor(vertices_vbo_layout_descriptor);
+
+    m_screen_vao->add_vertex_buffer(vertices_vbo);
+    m_screen_vao->set_index_buffer(index_buffer);
+    m_screen_vao->un_bind();
+}
+
+void bloom_app::setup_frame_buffers()
+{
+    // 1. Create geometry fbo.
+    glm::ivec2 viewport_size = retro::renderer::renderer::get_viewport_size();
+    {
+        std::vector<retro::renderer::frame_buffer_attachment> attachments = {
+            //  Position
+            {
+                retro::renderer::texture_format::rgba16f,
+                retro::renderer::texture_internal_format::rgba,
+                retro::renderer::texture_filtering::linear,
+                retro::renderer::texture_wrapping::clamp_to_edge,
+            },
+            // Albedo
+            {
+                retro::renderer::texture_format::rgba16f,
+                retro::renderer::texture_internal_format::rgba,
+                retro::renderer::texture_filtering::linear,
+                retro::renderer::texture_wrapping::clamp_to_edge,
+            },
+            // Normals
+            {
+                retro::renderer::texture_format::rgba16f,
+                retro::renderer::texture_internal_format::rgba,
+                retro::renderer::texture_filtering::linear,
+                retro::renderer::texture_wrapping::clamp_to_edge,
+            },
+            // Roughness Metallic AO
+            {
+                retro::renderer::texture_format::rgba16f,
+                retro::renderer::texture_internal_format::rgba,
+                retro::renderer::texture_filtering::linear,
+                retro::renderer::texture_wrapping::clamp_to_edge,
+            },
+            // Emissive
+            {
+                retro::renderer::texture_format::rgba16f,
+                retro::renderer::texture_internal_format::rgba,
+                retro::renderer::texture_filtering::linear,
+                retro::renderer::texture_wrapping::clamp_to_edge,
+            }};
+        retro::renderer::frame_buffer_attachment depth_attachment = {
+            retro::renderer::texture_format::depth_component32f,
+            retro::renderer::texture_internal_format::rgba,
+            retro::renderer::texture_filtering::linear,
+            retro::renderer::texture_wrapping::clamp_to_edge,
+        };
+        m_geometry_fbo = std::make_shared<retro::renderer::frame_buffer>(
+            attachments, viewport_size.x, viewport_size.y, depth_attachment);
+    }
+    {
+        std::vector<retro::renderer::frame_buffer_attachment> attachments = {
+            //  Position
+            {
+                retro::renderer::texture_format::rgba16f,
+                retro::renderer::texture_internal_format::rgba,
+                retro::renderer::texture_filtering::linear,
+                retro::renderer::texture_wrapping::clamp_to_edge,
+            },
+        };
+        m_lighting_fbo = std::make_shared<retro::renderer::frame_buffer>(attachments, viewport_size.x, viewport_size.y);
+    }
+}
+
 void bloom_app::on_handle_event(retro::events::base_event &event)
 {
+    retro::events::event_dispatcher dispatcher(event);
+    dispatcher.dispatch<retro::events::window_resize_event>(BIND_EVENT_FN(bloom_app::on_window_resize));
 }
 
 bool bloom_app::on_window_resize(retro::events::window_resize_event &resize_event)
 {
+    m_geometry_fbo->resize(resize_event.get_size());
+    m_lighting_fbo->resize(resize_event.get_size());
     return application::on_window_resize(resize_event);
 }
 
--- examples/bloom/src/bloom_app.h
+++ examples/bloom/src/bloom_app.h
@@ -11,19 +11,27 @@ public:
     void on_update() override;
 
     void load_shaders();
-    void load_texture();
     void setup_model();
     void setup_camera();
+    void setup_screen_quad();
+    void setup_frame_buffers();
 
-    void on_handle_event(retro::events::base_event& event) override;
-    bool on_window_resize(retro::events::window_resize_event& resize_event) override;
+    void on_handle_event(retro::events::base_event &event) override;
+    bool on_window_resize(retro::events::window_resize_event &resize_event) override;
 
 private:
     /* Common Variables */
     std::shared_ptr<retro::camera::camera> m_camera;
 
+    /* Rendering */
+    std::shared_ptr<retro::renderer::frame_buffer> m_geometry_fbo;
+    std::shared_ptr<retro::renderer::frame_buffer> m_lighting_fbo;
+    std::shared_ptr<retro::renderer::shader> m_geometry_shader;
+    std::shared_ptr<retro::renderer::shader> m_screen_shader;
+    std::shared_ptr<retro::renderer::shader> m_pbr_shader;
+    std::shared_ptr<retro::renderer::vertex_array_object> m_screen_vao;
+
     /* Model Variables */
-    std::shared_ptr<retro::renderer::texture> m_texture;
-    std::shared_ptr<retro::renderer::shader> m_shader;
+    std::shared_ptr<retro::renderer::material> m_material;
     std::shared_ptr<retro::renderer::model> m_model;
 };
